39b9e55efb7fe38061faa3fb004260532a9ea9b4
Loporrit <141286461+loporrit@users.noreply.github.com>
2023-09-01 15:02:24 +0000

    Add /auth/registerNewKey

--- MareSynchronosAPI/Routes/MareAuth.cs
+++ MareSynchronosAPI/Routes/MareAuth.cs
@@ -5,5 +5,7 @@ public class MareAuth
 
     public const string Auth = "/auth";
     public const string Auth_CreateIdent = "createWithIdent";
+    public const string Auth_Register = "registerNewKey";
     public static Uri AuthFullPath(Uri baseUri) => new Uri(baseUri, Auth + "/" + Auth_CreateIdent);
+    public static Uri AuthRegisterFullPath(Uri baseUri) => new Uri(baseUri, Auth + "/" + Auth_Register);
 }
